2301a5ff6b99489b3ef8cb8d8c3a2cfb0fc1b1d7
prashanthpanju <prashanth11391@gmail.com>
2020-04-06 08:49:19 +0530

    User Login / Register api code

--- auth/auth.js
+++ auth/auth.js
@@ -29,11 +29,12 @@ router.get(
         if (!err) {
           if (rows.length <= 0) {
             sql.query(
-              "INSERT INTO `users`(`uid`, `uname`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?)"
+              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)"
             ,
               [
                 req.user.id,
                 `${req.user._json.first_name} ${req.user._json.last_name}`,
+                '',
                 req.user._json.email,
                 "Facebook",
                 token
@@ -87,11 +88,12 @@ router.get(
         if (!err) {
           if (rows.length <= 0) {
             sql.query(
-              "INSERT INTO `users`(`uid`, `uname`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?)"
+              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)"
             ,
               [
                 req.user.id,
                 req.user._json.name,
+                '',
                 req.user._json.email,
                 "Google",
                 token
@@ -117,6 +119,7 @@ router.get(
   }
 );
 
+// Admin Login
 router.post("/login", (req, res) => {
   sql.query(
     `SELECT * from admin WHERE uname = ? and passcode = ?`,
@@ -144,6 +147,70 @@ router.post("/login", (req, res) => {
       }
     }
   );
+});
+
+// Customer Login
+/* Required Parameters 
+@username
+@password
+@logintype
+*/
+router.post("/userlogin", (req, res) => {
+  sql.query(
+    `SELECT uname from users WHERE uname = ? and upass = ? and logintype = ?`,
+    [req.body.userName, req.body.password, req.body.logintype],
+    (err, rows) => {
+      if (!err) {
+        if (rows.length > 0) {
+          const tokgen = new TokenGenerator(256, TokenGenerator.BASE71);
+          const token = tokgen.generate();
+          const updateTokenQuery = `UPDATE users SET token = ? where uname= ? and upass = ? and logintype = ?`;
+          sql.query(
+            updateTokenQuery,
+            [token, req.body.userName, req.body.password, req.body.logintype],
+            (err, rows) => {
+              if (!err) {
+                res.send({ token: token, authenticated: true });
+              }
+            }
+          );
+        } else {
+          res.send({ token: "error", authenticated: false });
+        }
+      } else {
+        res.send({ token: "error", authenticated: false });
+      }
+    }
+  );
+});
+
+
+router.post("/register", (req, res) => {
+  const uidtokgen = new TokenGenerator();
+  tokgen.generate();
+  const logintokgen = new TokenGenerator(256, TokenGenerator.BASE71);
+  const logintoken = logintokgen.generate();
+  let insQuery = 'INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)'
+  let checkUser = 'select * from users where uname = ? and email = ?';
+  sql.query(
+    checkUser,
+    [res.body.username, res.body.email],
+    (err, rows) => {
+      if(!err) {
+        if(rows.length === 0) {
+          sql.query(insQuery, [uidtokgen, res.body.name, res.body.password, res.body.email, 'web', logintoken], (err, rows) => {
+            if(!err) {
+              res.send({token: logintoken, authenticated: true});
+            } else {
+              res.send({authenticated: false});
+            }
+          })
+        }
+      } else {
+        res.send({error: 'Query Error'});
+      }
+    }
+  )
 });
 
 router.get("/failure", (req, res) => {
