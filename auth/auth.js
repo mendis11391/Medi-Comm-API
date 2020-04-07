18ff880e9e576798c4ff1a7f25a5a7f3c749d65f
prashanthpanju <prashanth11391@gmail.com>
2020-04-07 11:00:26 +0530

    Password encryption changes

--- auth/auth.js
+++ auth/auth.js
@@ -14,14 +14,17 @@ router.get(
   constants.facebookCallback,
   passport.authenticate("facebook", { failureRedirect: "/failure" }),
   function (req, res) {
-    const query = querystring.stringify({
-      email: req.user._json.email,
-      firstname: req.user._json.first_name,
-      lastname: req.user._json.last_name,
-      id: req.user.id,
-    });
+    // email: req.user._json.email,
+    // firstname: req.user._json.first_name,
+    // lastname: req.user._json.last_name,
+    // id: req.user.id
     const tokgen = new TokenGenerator(256, TokenGenerator.BASE71);
     const token = tokgen.generate();
+
+    var secretkey = crypto.createCipher("aes-128-cbc", "i#rent*out-api&key");
+    var fbId = secretkey.update(req.user.id, "utf8", "hex");
+    fbId += secretkey.final("hex");
+
     sql.query(
       `select * from users where email = ?`,
       [req.user._json.email],
@@ -29,30 +32,42 @@ router.get(
         if (!err) {
           if (rows.length <= 0) {
             sql.query(
-              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)"
-            ,
+              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)",
               [
-                req.user.id,
+                fbId,
                 `${req.user._json.first_name} ${req.user._json.last_name}`,
-                '',
+                "",
                 req.user._json.email,
                 "Facebook",
-                token
-              ], (err, rows) => {
+                token,
+              ],
+              (err, rows) => {
                 if (!err) {
-                  return res.redirect(`http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=false`);
+                  return res.redirect(
+                    `http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=false`
+                  );
                 } else {
-                  res.send({token: null, authenticated: false, newUser: true });
+                  res.send({
+                    token: null,
+                    authenticated: false,
+                    newUser: true,
+                  });
                 }
-              });
+              }
+            );
           } else {
             const updateTokenQuery = `UPDATE users SET token=? where uid= ? and email = ?`;
-            sql.query(updateTokenQuery, [token, req.user.id, req.user._json.email],
+            sql.query(
+              updateTokenQuery,
+              [token, req.user.id, req.user._json.email],
               (err, rows) => {
-                if(!err) {
-                  return res.redirect(`http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=true`);
+                if (!err) {
+                  return res.redirect(
+                    `http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=true`
+                  );
                 }
-              });
+              }
+            );
           }
         }
       }
@@ -74,13 +89,17 @@ router.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/failure" }),
   function (req, res) {
-    const query = querystring.stringify({
-      email: req.user._json.email,
-      firstname: req.user._json.name,
-      id: req.user.id,
-    });
+    // email: req.user._json.email,
+    // firstname: req.user._json.name,
+    // id: req.user.id
+
     const tokgen = new TokenGenerator(256, TokenGenerator.BASE71);
     const token = tokgen.generate();
+
+    var secretkey = crypto.createCipher("aes-128-cbc", "i#rent*out-api&key");
+    var gpId = secretkey.update(req.user.id, "utf8", "hex");
+    gpId += secretkey.final("hex");
+
     sql.query(
       `select * from users where email = ?`,
       [req.user._json.email],
@@ -88,30 +107,42 @@ router.get(
         if (!err) {
           if (rows.length <= 0) {
             sql.query(
-              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)"
-            ,
+              "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)",
               [
-                req.user.id,
+                gpId,
                 req.user._json.name,
-                '',
+                "",
                 req.user._json.email,
                 "Google",
-                token
-              ], (err, rows) => {
+                token,
+              ],
+              (err, rows) => {
                 if (!err) {
-                  return res.redirect(`http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=false`);
+                  return res.redirect(
+                    `http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=false`
+                  );
                 } else {
-                  res.send({token: null, authenticated: false, newUser: true });
+                  res.send({
+                    token: null,
+                    authenticated: false,
+                    newUser: true,
+                  });
                 }
-              });
+              }
+            );
           } else {
             const updateTokenQuery = `UPDATE users SET token=? where uid= ? and email = ?`;
-            sql.query(updateTokenQuery, [token, req.user.id, req.user._json.email],
+            sql.query(
+              updateTokenQuery,
+              [token, req.user.id, req.user._json.email],
               (err, rows) => {
-                if(!err) {
-                  return res.redirect(`http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=true`);
+                if (!err) {
+                  return res.redirect(
+                    `http://localhost:4200/dashboard/default?id=${req.user.id}&token=${token}&login=google&existing=true`
+                  );
                 }
-              });
+              }
+            );
           }
         }
       }
@@ -121,21 +152,27 @@ router.get(
 
 // Admin Login
 router.post("/login", (req, res) => {
+
+  var secretkey = crypto.createCipher('aes-128-cbc', 'i#rent*out-api&key');
+  var adpass = secretkey.update(req.body.password, 'utf8', 'hex')
+  adpass += secretkey.final('hex');
+
   sql.query(
     `SELECT * from admin WHERE uname = ? and passcode = ?`,
-    [req.body.userName, req.body.password],
+    [req.body.userName, adpass],
     (err, rows) => {
       if (!err) {
         if (rows.length > 0) {
+
           const tokgen = new TokenGenerator(256, TokenGenerator.BASE71);
           const token = tokgen.generate();
           const updateTokenQuery = `UPDATE admin SET token=? where uname= ? and passcode = ?`;
           sql.query(
             updateTokenQuery,
-            [token, req.body.userName, req.body.password],
+            [token, req.body.userName, adpass],
             (err, rows) => {
               if (!err) {
-                res.send({ token: token, authenticated: true });
+                res.send({ token: token, authenticated: true});
               }
             }
           );
@@ -196,29 +233,44 @@ router.post("/register", (req, res) => {
   const userToken = `irentout-${uidtokgen.generate()}`;
   const logintokgen = new TokenGenerator(256, TokenGenerator.BASE71);
   const logintoken = logintokgen.generate();
-  let insQuery = 'INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)'
-  let checkUser = 'select * from users where uname = ? and email = ?';
-  sql.query(
-    checkUser,
-    [req.body.username, req.body.email],
-    (err, rows) => {
-      if(!err) {
-        if(rows.length === 0) {
-          sql.query(insQuery, [userToken, req.body.username, req.body.password, req.body.email, 'web', logintoken], (err, rows) => {
-            if(!err) {
-              res.send({status: 'Successfully Registered', token: logintoken, authenticated: true});
+  let insQuery =
+    "INSERT INTO `users`(`uid`, `uname`, `upass`, `email`, `logintype`, `token`) VALUES (?, ?, ?, ?, ?, ?)";
+  let checkUser = "select * from users where uname = ? and email = ?";
+  sql.query(checkUser, [req.body.username, req.body.email], (err, rows) => {
+    if (!err) {
+      if (rows.length === 0) {
+        sql.query(
+          insQuery,
+          [
+            userToken,
+            req.body.username,
+            req.body.password,
+            req.body.email,
+            "web",
+            logintoken,
+          ],
+          (err, rows) => {
+            if (!err) {
+              res.send({
+                status: "Successfully Registered",
+                token: logintoken,
+                authenticated: true,
+              });
             } else {
-              res.send({authenticated: false});
+              res.send({ authenticated: false });
             }
-          })
-        } else {
-          res.send({status: 'User with this emil is already registered', authenticated: false});
-        }
+          }
+        );
       } else {
-        res.send({error: 'Query Error'});
+        res.send({
+          status: "User with this emil is already registered",
+          authenticated: false,
+        });
       }
+    } else {
+      res.send({ error: "Query Error" });
     }
-  )
+  });
 });
 
 router.get("/failure", (req, res) => {
